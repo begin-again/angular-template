4487e911d310bb40f6237483e4838ef5df8fd27d
Todd Hogarth <begin-again@users.noreply.github.com>
2020-03-02 08:27:04 -0500

    add env

--- karma.conf.js
+++ karma.conf.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
